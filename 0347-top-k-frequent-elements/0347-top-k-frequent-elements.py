04aa8ebc552ef17bea2a9a0bfcb369496ba28007
Akshay Kotha <70824397+akotha12@users.noreply.github.com>
2023-01-10 12:30:32 -0500

    Time: 73 ms (97.62%), Space: 16.8 MB (61.54%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        myMap = {}
+        max = 0
+        output = []
+        for i in range(len(nums)):
+            if myMap.has_key(nums[i]):
+                myMap[nums[i]] += 1
+            else:
+                myMap[nums[i]] = 1
+        frequency = sorted(myMap.items(), key=lambda x:x[1])
+        tmp = frequency[-k:]
+        print(tmp);
+        for value in tmp:
+            output.append(int(value[0]))
+        print(output)
+        return output
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
