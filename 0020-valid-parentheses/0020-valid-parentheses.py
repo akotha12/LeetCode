cc20f2cf2ecc3011150077be6d2329b46f7f5cf8
Akshay Kotha <70824397+akotha12@users.noreply.github.com>
2023-01-15 01:54:40 -0500

    Time: 19 ms (77.94%), Space: 13.7 MB (35.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        stack = deque()
-        for i in range(len(s)):
-            if (s[i] == "(" or s[i] == "[" or s[i] == "{"):
-                stack.append(s[i])
-            elif (len(stack) > 0):
-                if ((s[i] == ')' and stack[-1] == '(') or (s[i] == ']' and stack[-1] == '[') or (s[i] == '}' and stack[-1] == '{')):
-                    stack.pop()
-                else:
-                    return False
-            else:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
                 return False
-        return len(stack) == 0
+            stack.pop()
+
+        return not stack
         """
         :type s: str
         :rtype: bool
