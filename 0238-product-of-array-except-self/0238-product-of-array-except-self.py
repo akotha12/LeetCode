9aaadaf6516da7b88133f33833dea88e8adb17a1
Akshay Kotha <70824397+akotha12@users.noreply.github.com>
2023-01-18 13:57:20 -0500

    Time: 184 ms (88.38%), Space: 20 MB (91.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -9,18 +9,14 @@ class Solution(object):
             else:
                 total2 *= nums[i]
             total *= nums[i]
-            print(total)
-            print(total2)
         for i in range(len(nums)):
             if (nums[i] == 0 and numZero < 2):
                 nums[i] = 1
                 nums[i] = total2
-                print('n = ', nums[i])
             elif (nums[i] == 0 and numZero > 1):
                 nums[i] = 0
             else:
                 nums[i] = total / nums[i]
-                print(nums[i])
         return nums
         """
         :type nums: List[int]
