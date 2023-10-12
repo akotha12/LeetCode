2a10c6c315b6e6b989682e58d3120571a3d718b6
Akshay Kotha <70824397+akotha12@users.noreply.github.com>
2023-10-12 12:15:23 -0400

    Time: 179 ms (75.32%), Space: 16.4 MB (52.53%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -0,0 +1,51 @@
+class BrowserHistory(object):
+
+    def __init__(self, homepage):
+        """
+        :type homepage: str
+        """
+        self.history = [homepage]
+        self.counter = 0
+        self.len = 1
+        
+
+    def visit(self, url):
+        """
+        :type url: str
+        :rtype: None
+        """
+        temp = self.history[0:self.counter + 1]
+        self.history = temp
+        self.history.append(url)
+        self.counter += 1
+        self.len = len(self.history)
+        
+
+    def back(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+        self.counter -= steps
+        if self.counter < 0:
+            self.counter = 0
+        return self.history[self.counter]
+        
+
+    def forward(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+        self.counter += steps
+        if self.counter >= self.len:
+            self.counter = self.len - 1
+        return self.history[self.counter]
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
